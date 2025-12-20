f34cdddd941a087f0dd07aa692acb9941b3af030
JhonatasMJ <jhonatasjhmj@gmail.com>
2025-12-20 08:43:28 -0300

    feat: Layout font config

---
+++ src/app/_layout.tsx
@@ -0,0 +1,34 @@
+import { colors } from "@/theme/colors";
+import { Stack } from "expo-router";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+
+/* Pega todas as rotas da pasta app e passa para o Slot */
+/* Posso definir diretamente o tipo de navegação 'Tabs' ou 'Stack' */
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold,
+  });
+
+    if (!fontsLoaded) { 
+        return null;
+    }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: {
+          backgroundColor: colors.white,
+        },
+      }}
+    />
+  );
+}
